218a6024d66a291e773782db0091155ccd83f1ba
hdlugas <73852653+hdlugas@users.noreply.github.com>
2025-01-23 22:42:51 -0500

    Add files via upload

---
+++ KAN_toy_example/scripts/evaluate_on_test_data/KAN.py
@@ -0,0 +1,49 @@
+
+from sklearn.model_selection import RepeatedStratifiedKFold
+from kan import *
+import os
+import sys
+
+parent_dir = os.path.abspath(os.path.join(os.path.dirname(__file__), '..'))
+sys.path.append(parent_dir)
+from functions import *
+
+def train_acc():
+    return torch.mean((torch.argmax(model_kan(dataset['train_input']), dim=1) == dataset['train_label']).float())
+
+def test_acc():
+    return torch.mean((torch.argmax(model_kan(dataset['test_input']), dim=1) == dataset['test_label']).float())
+
+x_train, y_train, x_test, y_test = get_test_train_data(path='/home/hunter/KAN_toy_example/data/raw_toy_data.csv')
+
+x_train = torch.from_numpy(x_train).float()
+y_train = torch.from_numpy(y_train.astype(np.int64))
+x_test = torch.from_numpy(x_test).float()
+y_test = torch.from_numpy(y_test.astype(np.int64))
+
+df_opt_params = pd.read_csv(f'/home/hunter/KAN_toy_example/data/hyperparameter_search_results/KAN_opt_params.csv')
+
+for i in range(0,df_opt_params.shape[0]):
+    num_epochs = int(df_opt_params['NUM.EPOCHS'][i])
+    grid = int(df_opt_params['GRID'][i])
+    k = int(df_opt_params['K'][i])
+    n_neurons = int(df_opt_params['N.NEURONS'][i])
+
+    if n_neurons == 0:
+        model_kan = KAN(width=[x_train.shape[1],2], grid=grid, k=k, seed=1)
+    else:
+        model_kan = KAN(width=[x_train.shape[1],n_neurons,2], grid=grid, k=k, seed=1)
+
+    dataset = {'train_input':x_train, 'train_label':y_train, 'test_input':x_test, 'test_label':y_test}
+    model_kan(dataset['train_input'])
+    results = model_kan.fit(dataset, opt='Adam', steps=num_epochs, metrics=(train_acc,test_acc), loss_fn=torch.nn.CrossEntropyLoss())
+    preds = model_kan(dataset['test_input'])
+    _, predicted_labels = torch.max(preds,1)
+    probs = softmax(preds.detach().numpy())[:,1]
+    pred_labels = predicted_labels.numpy().tolist()
+
+    df_out = pd.DataFrame({'PRED':pred_labels, 'TRUE':y_test.numpy(), 'PROB.CASE':probs})
+    path_out = f'/home/hunter/KAN_toy_example/data/test_data_evaluation/KAN_num_epochs_{num_epochs}_grid_{grid}_k_{k}_n_neurons_{n_neurons}.csv'
+    df_out.to_csv(path_out, index=False)
+
+
---
+++ KAN_toy_example/scripts/hyperparameter_grid_search/FNN.py
@@ -0,0 +1,56 @@
+
+from functions import *
+from sklearn.model_selection import RepeatedStratifiedKFold
+import keras
+from keras.models import Sequential
+from keras.layers import Dense
+from keras.optimizers import Adam
+from tensorflow.keras import models
+
+x_train_original, y_train_original, x_test_original, y_test_original = get_test_train_data(path='/home/hunter/KAN_toy_example/data/raw_toy_data.csv')
+
+num_epochs_range = [100]
+lr_range = [0.01]
+n_neurons_range = [5,10]
+act_funcs = ['sigmoid']
+
+rskf_cv = RepeatedStratifiedKFold(n_splits=5, n_repeats=10, random_state=1)
+
+for num_epochs in num_epochs_range:
+    for lr in lr_range:
+        for n_neurons in n_neurons_range:
+            for act_func in act_funcs:
+                idx = -1
+                idxs = []
+                true_labels = []
+                pred_labels = []
+                probs = []
+                for train_idx, test_idx in rskf_cv.split(x_train_original, y_train_original):
+                    idx = idx + 1
+                    print(f'\nIteration #{idx}')
+
+                    x_train, x_test = x_train_original[train_idx], x_train_original[test_idx]
+                    y_train, y_test = y_train_original[train_idx], y_train_original[test_idx]
+                        
+                    model_ann = models.Sequential()
+                    if n_neurons > 0:
+                        model_ann.add(Dense(n_neurons, activation=act_func, input_dim=x_train.shape[1]))
+                        model_ann.add(Dense(1, activation='sigmoid'))
+                    else:
+                        model_ann.add(Dense(1, activation='sigmoid', input_dim=x_train.shape[1]))
+
+                    model_ann.compile(optimizer=Adam(learning_rate=lr), loss='binary_crossentropy', metrics=[keras.metrics.AUC])
+                    model_ann.fit(x_train, y_train, epochs=num_epochs, batch_size=None, verbose=0)
+                    probs_tmp = model_ann.predict(x_test).flatten()
+                    y_preds = [1 if prob >= 0.5 else 0 for prob in probs_tmp]
+
+                    probs = probs + probs_tmp.tolist()
+                    pred_labels = pred_labels + y_preds
+                    true_labels = true_labels + y_test.tolist()
+                    idxs = idxs + [idx] * len(probs_tmp)
+
+                df_out = pd.DataFrame({'ITERATION':idxs, 'PRED':pred_labels, 'TRUE':true_labels, 'PROB.CASE':probs})
+                path_out = f'/home/hunter/KAN_toy_example/data/hyperparameter_search_results/FNN_num_epochs_{num_epochs}_act_func_{act_func}_lr_{lr}_n_neurons_{n_neurons}.csv'
+                df_out.to_csv(path_out, index=False)
+
+
